60ac6bb96420d9fb4b626f44456d2ec061c18536
jasperbosch <jasperbosch@chello.nl>
2021-12-16 08:06:13 +0100

    day15:niet compleet uitgevoerd

--- day15/index.ts
+++ day15/index.ts
@@ -9,9 +9,146 @@ function readFile(): string[] {
     return input;
 }
 
+interface Risk {
+    coord: string,
+    value: number,
+    left?: string,
+    right?: string
+}
+
+
+const tree: Map<string, Risk> = new Map<string, Risk>();
+const unvisited: string[] = [];
+const distances: Map<string, number> = new Map<string, number>();
+
+function procesDistance(key: string) {
+    const node = tree.get(key);
+    if (node) {
+        const currDistance = distances.get(key);
+        if (currDistance !== undefined) {
+            // distances[key] = 0;
+
+            if (node.left) {
+                const nodeA = tree.get(node.left);
+                distances.set(node.left, Math.min(distances.get(node.left)!, currDistance + +nodeA!.value));
+            }
+            if (node.right) {
+                const nodeB = tree.get(node.right);
+                distances.set(node.right, Math.min(distances.get(node.right)!, currDistance + +nodeB!.value));
+            }
+
+            unvisited.splice(unvisited.indexOf(node.coord), 1);
+            const size = unvisited.length;
+            if (size % 100 === 0) {
+                console.log(unvisited.length, distances.size);
+            }
+        }
+    }
+}
+
+function findSmallestUnvisitedNode() {
+    let minValue = Infinity;
+    let minValueProp = '-1';
+    for (let x = 0; x < unvisited.length; x++) {
+        const value = distances.get(unvisited[x]);
+        if (value && value < minValue) {
+            minValue = value;
+            minValueProp = unvisited[x];
+        }
+    }
+    // const filterdDistances = [...distances].filter(([k, v]) => v < Infinity);
+    // unvisited.filter(v => filterdDistances.find(value => value[0] === v)).forEach(entry => {
+    //     const prop = entry;
+    //     if (distances.get(prop)! < minValue) {
+    //         minValue = distances.get(prop)!;
+    //         minValueProp = prop;
+    //     }
+    // });
+
+    return minValueProp;
+}
+
+function printmatrix() {
+    matrix.forEach(line => {
+        console.log(line.join(''));
+    });
+    console.log('---------------------------------------------');
+}
+
+let matrix: number[][] = [];
 
 function main() {
     const lines = readFile();
+
+    lines.forEach((line, lindex) => {
+        matrix.push([]);
+        matrix[lindex] = line.split('').map(value => +value);
+    });
+
+    const mLenght = matrix.length;
+    const lLength = matrix[0].length;
+
+
+    const size = matrix.length;
+
+    // printmatrix();
+
+    for (let lindex = 0; lindex < size; lindex++) {
+        matrix[lindex] = [
+            ...JSON.parse(JSON.stringify(matrix[lindex])),
+            ...JSON.parse(JSON.stringify(matrix[lindex])),
+            ...JSON.parse(JSON.stringify(matrix[lindex])),
+            ...JSON.parse(JSON.stringify(matrix[lindex])),
+            ...JSON.parse(JSON.stringify(matrix[lindex]))
+        ];
+    }
+    matrix = [
+        ...JSON.parse(JSON.stringify(matrix)),
+        ...JSON.parse(JSON.stringify(matrix)),
+        ...JSON.parse(JSON.stringify(matrix)),
+        ...JSON.parse(JSON.stringify(matrix)),
+        ...JSON.parse(JSON.stringify(matrix))
+    ];
+
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+            for (let y = i * mLenght; y < (i + 1) * mLenght; y++) {
+                for (let x = j * lLength; x < (j + 1) * lLength; x++) {
+                    matrix[y][x] += i + j;
+                    if (matrix[y][x] > 9) matrix[y][x] = matrix[y][x] % 9;
+                }
+            }
+        }
+    }
+
+    // printmatrix();
+
+    matrix.forEach((line, lindex) => {
+        line.forEach((value, vindex) => {
+            let left, right;
+            if (lindex + 1 < matrix.length) left = vindex + ',' + (lindex + 1);
+            if (vindex + 1 < line.length) right = (vindex + 1) + ',' + lindex;
+            tree.set(vindex + ',' + lindex, {
+                coord: vindex + ',' + lindex,
+                value: value,
+                left: left,
+                right: right
+            });
+            unvisited.push(vindex + ',' + lindex);
+            distances.set(vindex + ',' + lindex, Infinity);
+        });
+    });
+
+    distances.set('0,0', 0);
+    unvisited.shift();
+
+    procesDistance('0,0');
+    while (unvisited.length > 0) {
+        const currentNode = findSmallestUnvisitedNode();
+        procesDistance(currentNode);
+    }
+
+    console.log(distances);
 
 
 }
